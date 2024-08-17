c7a46d19d42ff91064e958ede87ccbfd8a5b3c89
LOGIKA <danilltrotsenko2002@gmail.com>
2024-08-17 13:30:33 +0300

    1

--- mapmanager.py
+++ mapmanager.py
@@ -1,18 +1,19 @@
 class Mapmanager:
     def __init__(self):
-        self.model = 'box'
-        # self.texture = 'box'
+        self.model = 'block.egg'
+        self.texture = 'block.png'
         self.color = (0.2, 0.2, 0.35, 1)
         self.land = None
         self.startNew()
         self.addBlock((0, 0, 0))
+        self.addBlock((0, 1, 0))
 
     def startNew(self):
         self.land = render.attachNewNode("Land")
 
     def addBlock(self, position):
         block = loader.loadModel(self.model)
-        # block.setTexture(loader.loadTexture(self.texture))
+        block.setTexture(loader.loadTexture(self.texture))
         block.setPos(position)
         block.setColor(self.color)
         block.reparentTo(self.land)
